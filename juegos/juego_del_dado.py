da598bdcc82d42bc7eeee28cf2186e86464e4538
sebastianekdahl <163366887+sebastianekdahl@users.noreply.github.com>
2024-03-14 13:50:56 +0000

    Termine el juego juego_del_dado.py

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -5,4 +5,21 @@ def juego_del_dado():
     Después el computador también tiene que lanzar un dado.
     El primero en sumar 30 puntos gana.
     """
-    pass
+    import random
+    contador_usuario = 0
+    contador_computador = 0
+    while contador_usuario < 30 and contador_computador < 30:
+        adelante = input("Tira el dado ")
+        numero_usuario = random.randint(1,6)
+        print("Tu numero es:",numero_usuario)
+        contador_usuario += numero_usuario
+        if contador_usuario < 30:
+            numero_computador = random.randint(1,6)
+            print("El numero del computador es:",numero_computador)
+            contador_computador += numero_computador
+        else:
+            print("Le ganaste al computador! Llegaste a 30 puntos")
+    if contador_computador >= 30:
+        print("Fuiste derrotado por el computador")
+
+juego_del_dado()
